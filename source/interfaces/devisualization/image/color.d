7a2ea7e3697237bb58f2f7469d231ef8869eaa89
rikki cattermole <pbdadmin@gmail.com>
2014-12-08 21:47:22 +1300

    Not float input gah

--- source/interfaces/devisualization/image/color.d
+++ source/interfaces/devisualization/image/color.d
@@ -84,7 +84,10 @@ class Color_RGBA {
     }
 
 	static Color_RGBA fromUbyte(ubyte r, ubyte g, ubyte b, ubyte a) {
-		return new Color_RGBA(r * ubyteToUshort, g * ubyteToUshort, b * ubyteToUshort, a * ubyteToUshort);
+		return new Color_RGBA(cast(ubyte)(r * ubyteToUshort),
+		                      cast(ubyte)(g * ubyteToUshort),
+		                      cast(ubyte)(b * ubyteToUshort),
+		                      cast(ubyte)(a * ubyteToUshort));
 	}
 }
 
