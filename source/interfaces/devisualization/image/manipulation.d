c359ca7e0680d860748890b25ff4225228212683
rikki cattermole <pbdadmin@gmail.com>
2015-04-03 01:02:00 +1300

    Untested, but increases the speed of flip* functions. Tested at ~12ms using dmd on release mode.

--- source/interfaces/devisualization/image/manipulation.d
+++ source/interfaces/devisualization/image/manipulation.d
@@ -2,6 +2,16 @@
 import devisualization.image.color;
 import devisualization.image.image;
 
+private __gshared {
+	/*
+	 * For usage with flip* functions.
+	 * 
+	 * Is used instead of ranges. Ranges are used when allocation becomes quite large.
+	 * Data itself is not used. It is only sliced.
+	 */
+	static ubyte[ushort.max] STATICBUFFER;
+}
+
 /**
  * Acts as a foreach over the pixels based upon left to right.
  * 
@@ -47,27 +57,65 @@ void applyByY(Image image, void delegate(Color_RGBA pixel, size_t x, size_t y) d
  * 		_	=	The image to flip
  */
 @property void flipVertical(Image _) {
+	import std.parallelism : parallel;
+
 	auto __ = _.rgba;
 	size_t width = _.width;
 	size_t height = _.height;
-	
+	size_t length = __.length;
+
 	size_t div2Height = height / 2;
-	
-	size_t offset;
-	foreach(x; 0 .. width) {
-		size_t i = offset;
-		size_t i2 = i + (height - 1);
-		
-		foreach(y; 0 .. div2Height) {
-			Color_RGBA to = __[i2];
-			__[i2] = __[i];
-			__[i] = to;
+
+	if (height > STATICBUFFER.length) {
+		struct TR {			
+			size_t offset;
 			
-			i++;
-			i2--;
+			@property {
+				size_t front() {
+					return offset;
+				}
+				
+				bool empty() {
+					return offset >= length;
+				}
+			}
+			
+			void popFront() {
+				offset += height;
+			}
 		}
 		
-		offset += width;
+		foreach(y, offset; parallel(TR())) {
+			size_t i = offset;
+			size_t i2 = i + (width - 1);
+			
+			foreach(x; 0 .. div2Height) {
+				Color_RGBA to = __[i2];
+				__[i2] = __[i];
+				__[i] = to;
+				
+				i++;
+				i2--;
+			}
+		}
+	} else {
+		size_t offset;
+
+		foreach(y, yv; parallel(STATICBUFFER[0 .. width])) {
+			size_t i = offset;
+			size_t i2 = i + (width - 1);
+			
+			foreach(x; 0 .. div2Height) {
+				Color_RGBA to = __[i2];
+				__[i2] = __[i];
+				__[i] = to;
+				
+				i++;
+				i2--;
+			}
+
+			offset += height;
+		}
 	}
 }
 
@@ -78,27 +126,65 @@ void applyByY(Image image, void delegate(Color_RGBA pixel, size_t x, size_t y) d
  * 		_	=	The image to flip
  */
 @property void flipHorizontal(Image _) {
+	import std.parallelism : parallel;
+
 	auto __ = _.rgba;
 	size_t width = _.width;
 	size_t height = _.height;
+	size_t length = __.length;
 
 	size_t div2Width = width / 2;
 
-	size_t offset;
-	foreach(y; 0 .. height) {
-		size_t i = offset;
-		size_t i2 = i + (width - 1);
+	if (height > STATICBUFFER.length) {
+		struct TR {			
+			size_t offset;
 
-		foreach(x; 0 .. div2Width) {
-			Color_RGBA to = __[i2];
-			__[i2] = __[i];
-			__[i] = to;
-
-			i++;
-			i2--;
+			@property {
+				size_t front() {
+					return offset;
+				}
+				
+				bool empty() {
+					return offset >= length;
+				}
+			}
+			
+			void popFront() {
+				offset += width;
+			}
 		}
 
-		offset += width;
+		foreach(y, offset; parallel(TR())) {
+			size_t i = offset;
+			size_t i2 = i + (width - 1);
+			
+			foreach(x; 0 .. div2Width) {
+				Color_RGBA to = __[i2];
+				__[i2] = __[i];
+				__[i] = to;
+				
+				i++;
+				i2--;
+			}
+		}
+	} else {
+		size_t offset;
+
+		foreach(y, yv; parallel(STATICBUFFER[0 .. height])) {
+			size_t i = offset;
+			size_t i2 = i + (width - 1);
+			
+			foreach(x; 0 .. div2Width) {
+				Color_RGBA to = __[i2];
+				__[i2] = __[i];
+				__[i] = to;
+				
+				i++;
+				i2--;
+			}
+
+			offset += width;
+		}
 	}
 }
 
